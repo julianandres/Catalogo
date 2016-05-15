c4cb14b109f5d0291a73e6bf22919d025d4eed03
JULIAN ANDRES <JULIAN ANDRES>
2016-05-14 22:31:12 -0500

    segundo commit

--- app/src/main/java/com/example/julian/catalogo/LoginActivity.java
+++ app/src/main/java/com/example/julian/catalogo/LoginActivity.java
@@ -2,12 +2,36 @@ package com.example.julian.catalogo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
+    Button btnIniciar;
+    Button btnRegistro;
+    EditText login;
+    EditText password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnIniciar = (Button) findViewById(R.id.btnLogin);
+        btnRegistro= (Button) findViewById(R.id.btnRegister);
+        login= (EditText) findViewById(R.id.txtUser);
+        password= (EditText) findViewById(R.id.txtPass);
+        btnIniciar.setOnClickListener(this);
+        btnRegistro.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (R.id.btnLogin==v.getId()){
+
+        }
+        if(R.id.btnRegister==v.getId()){
+
+        }
     }
 }
--- app/src/main/java/com/example/julian/catalogo/models/Usuario.java
+++ app/src/main/java/com/example/julian/catalogo/models/Usuario.java
@@ -9,17 +9,26 @@ public class Usuario {
     private String login;
     private String password;
     private int  nivelUsuario;
+    private String name;
+    private int telefono;
+    private String correo;
+    private String genero;
 
-    public Usuario(String idUsuario, String login, String password, int nivelUsuario) {
+    public Usuario(String idUsuario, String login, String password, int nivelUsuario, String name, int telefono, String correo, String genero) {
         this.idUsuario = idUsuario;
         this.login = login;
         this.password = password;
         this.nivelUsuario = nivelUsuario;
+        this.name = name;
+        this.telefono = telefono;
+        this.correo = correo;
+        this.genero = genero;
     }
 
     public String getIdUsuario() {
         return idUsuario;
     }
+
     public void setIdUsuario(String idUsuario) {
         this.idUsuario = idUsuario;
     }
@@ -27,6 +36,7 @@ public class Usuario {
     public String getLogin() {
         return login;
     }
+
     public void setLogin(String login) {
         this.login = login;
     }
@@ -34,6 +44,7 @@ public class Usuario {
     public String getPassword() {
         return password;
     }
+
     public void setPassword(String password) {
         this.password = password;
     }
@@ -41,8 +52,41 @@ public class Usuario {
     public int getNivelUsuario() {
         return nivelUsuario;
     }
+
     public void setNivelUsuario(int nivelUsuario) {
         this.nivelUsuario = nivelUsuario;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
     }
 }
 
